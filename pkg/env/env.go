ea84c9ca4d7b2af210d43e6f4a6f69c668b6d6a2
shinnosuke kishida <shin.050195@gmail.com>
2021-12-26 23:32:24 +0900

    環境変数を使用する処理を追加 (#2)

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -1,6 +1,32 @@
 package env
 
-func Load() error {
+import (
+	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+const (
+	envFile = ".env"
+)
+
+var (
+	app = &AppEnv{}
+)
+
+func Get() *AppEnv {
+	return app
+}
+
+func Load() error {
+	if err := godotenv.Load(filepath.Join(os.Getenv("PWD"), envFile)); err != nil {
+		return fmt.Errorf("failed to load .env. %w", err)
+	}
+	if err := envconfig.Process("", app); err != nil {
+		return fmt.Errorf("failed to mapping env. %w", err)
+	}
 	return nil
 }
