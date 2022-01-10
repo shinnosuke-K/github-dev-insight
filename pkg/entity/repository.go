b66662bee98495f2ea2973d9d1b3d3b20282b561
shinnosuke-K <shinnosuke.kishida@so-tech.co.jp>
2022-01-10 12:46:14 +0900

    取得状況のフィールドを追加

--- pkg/entity/repository.go
+++ pkg/entity/repository.go
@@ -9,16 +9,18 @@ type RepositoryID string
 type GitHubID string
 
 type Repository struct {
-	ID          RepositoryID `json:"id"`
-	GitHubID    GitHubID     `json:"github_id"`
-	Owner       string       `json:"owner"`
-	Name        string       `json:"name"`
-	Description string       `json:"description"`
-	TotalPR     int64        `json:"total_pr"`
-	TotalIssue  int64        `json:"total_issue"`
-	CreatedAt   time.Time    `json:"created_at"`
-	UpdatedAt   time.Time    `json:"updated_at"`
-	PushedAt    *time.Time   `json:"pushed_at"`
+	ID             RepositoryID `json:"id"`
+	GitHubID       GitHubID     `json:"github_id"`
+	Owner          string       `json:"owner"`
+	Name           string       `json:"name"`
+	Description    string       `json:"description"`
+	TotalPR        int64        `json:"total_pr"`
+	TotalIssue     int64        `json:"total_issue"`
+	GetPullRequest bool         `json:"get_pull_request"`
+	GetIssue       bool         `json:"get_issue"`
+	CreatedAt      time.Time    `json:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at"`
+	PushedAt       *time.Time   `json:"pushed_at"`
 }
 
 type Repositories []*Repository
