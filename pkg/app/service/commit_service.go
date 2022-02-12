3bc01e199e8bf56b0fea3dddd9720fba1baf519a
shinnosuke kishida <shinnosuke.kishida@so-tech.co.jp>
2022-02-12 14:37:20 +0900

    commit情報を保存するの処理を追加 (#23)
    * commit情報を保存するの処理を追加
    
    * DBのコネクションを並行処理ごとに作るように修正

--- pkg/app/service/commit_service.go
+++ pkg/app/service/commit_service.go
@@ -7,6 +7,7 @@ import (
 	"github.com/shinnosuke-K/github-dev-insight/pkg/adapter"
 	datastoreParams "github.com/shinnosuke-K/github-dev-insight/pkg/adapter/datastore/params"
 	"github.com/shinnosuke-K/github-dev-insight/pkg/adapter/github/params"
+	"github.com/shinnosuke-K/github-dev-insight/pkg/entity"
 )
 
 type CommitService interface {
@@ -36,20 +37,38 @@ func (s *commitService) ImportCommit(ctx context.Context) error {
 		if len(prs) == 0 {
 			break
 		}
-
-		for _, p := range prs {
-			if p.TotalCommits == 0 {
-				continue
-			}
-			cm, err := s.GitHub().GetCommitsByGitHubID(ctx, &params.GetCommitsByGitHubID{
-				GitHubID:      p.GitHubID,
-				PullRequestID: p.ID,
-			})
-			if err != nil {
-				return fmt.Errorf("failed to get commit by id:[%s]. %w", prs[0].GitHubID, err)
-			}
-			fmt.Println(cm)
+		if err := s.store(ctx, prs); err != nil {
+			return fmt.Errorf("failed to store. %w", err)
 		}
+	}
+	return nil
+}
+
+func (s *commitService) store(ctx context.Context, prs []*entity.PullRequest) error {
+	for _, p := range prs {
+		if p.TotalCommits == 0 {
+			continue
+		}
+		p := p
+		cm, err := s.GitHub().GetCommitsByGitHubID(ctx, &params.GetCommitsByGitHubID{
+			GitHubID:      p.GitHubID,
+			PullRequestID: p.ID,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to get commit by id:[%s]. %w", p.GitHubID, err)
+		}
+		if err := s.DataStore().Transaction(ctx, func(ctx context.Context) error {
+			if err := s.DataStore().Commit().Create(ctx, cm...); err != nil {
+				return fmt.Errorf("failed to create commit. %w", err)
+			}
+			if err := s.DataStore().PullRequest().UpdateStatusByID(ctx, p.ID, true); err != nil {
+				return fmt.Errorf("failed to update pull request by id:[%s]. %w", p.ID, err)
+			}
+			return nil
+		}); err != nil {
+			return fmt.Errorf("error occurred in transaction. %w", err)
+		}
+		fmt.Printf("[info]: pr[%s] done\n", p.Title)
 	}
 	return nil
 }
